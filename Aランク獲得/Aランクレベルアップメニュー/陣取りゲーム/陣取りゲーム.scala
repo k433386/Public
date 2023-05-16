26139ab047412d132e208eec33c5b43d30d463a7
k433386 <k433386@kansai-u.ac.jp>
2023-05-16 22:36:13 +0900

    test
    test

--- Aランク獲得/Aランクレベルアップメニュー/陣取りゲーム/陣取りゲーム.scala
+++ Aランク獲得/Aランクレベルアップメニュー/陣取りゲーム/陣取りゲーム.scala
@@ -1,10 +1,125 @@
 import scala.io.StdIn._
+import scala.collection.mutable.Queue
+import math.abs
 
 object Main extends App {
+ 
+    val line = readLine().split(" ").map(_.toInt)
+    val H = line(0)
+    val W = line(1)
+    val N = readLine()
+    val Sh = Array.ofDim[String](H, W)
 
-    val ab = readLine().split(" ").map(_.toInt)
-    val s = readLine()
+    val countMA : Int = N match {
+        case "A" => { 10000 }
+        case "B" => { 20000 }
+    }
+    val countMB : Int = N match {
+        case "A" => { 20000 }
+        case "B" => { 10000 }
+    }
 
-    val result = s.drop(ab(0)-1).take(ab(1)-ab(0)+1)
-    println(result)
+    for (i <- 0 until H){
+        Sh(i) = readLine().split("")
+    }
+
+    def AstartSearch(board: Array[Array[String]]): (Int, Int) = {
+        for (i <- 0 until H; j <- 0 until W){
+            if (board(i)(j) == "A") {
+                return (i, j)
+            }
+        }
+        return (-1, -1)
+    }
+    def BstartSearch(board: Array[Array[String]]): (Int, Int) = {
+        for (i <- 0 until H; j <- 0 until W){
+            if (board(i)(j) == "B") {
+                return (i, j)
+            }
+        }
+        return (-1, -1)
+    }
+    def inMap(y: Int, x: Int) : Boolean = {
+        (0 <= y && y < H && 0 <= x && x < W)
+    }
+    def noObject(board: Array[Array[String]], y: Int, x: Int) : Boolean = {
+        (board(y)(x) != "#")
+    }
+
+    def nextToPlot(board: Array[Array[String]], aTup: (Int, Int), bTup: (Int, Int), rank: (Int, Int)): Array[Array[String]] = {
+        val (ay, ax) = aTup
+        val (by, bx) = bTup
+        val (countA, countB) = rank
+        board(ay)(ax) = countA.toString
+        board(by)(bx) = countB.toString
+        val queueA = Queue[(Int, Int, Int)]((ay, ax, countA))
+        val queueB = Queue[(Int, Int, Int)]((by, bx, countB))
+        while (queueA.nonEmpty && queueB.nonEmpty) {
+            val (ay, ax, countA) = queueA.dequeue()
+            val (by, bx, countB) = queueB.dequeue()
+            def nextSub(y: Int, x: Int, z: Queue[(Int, Int, Int)], w: Int) = {
+                if (inMap(y, x)){
+                    if (noObject(board, y, x)){
+                        val t1 = board(y)(x)
+                        val t2 = w + 1
+                        if (t1 == "."){
+                            board(y)(x) = s"${t2}"
+                            z.enqueue((y, x, t2))
+                        }else if (t1.toInt%10000 >= t2%10000 && t1.toInt > t2 && abs(t1.toInt-t2)>=10000){
+                            board(y)(x) = s"${t2}"
+                            z.enqueue((y, x, t2))
+                        }
+                    }
+                }
+            }
+            def callA() = {
+                nextSub(ay-1, ax, queueA, countA)
+                nextSub(ay+1, ax, queueA, countA)
+                nextSub(ay, ax-1, queueA, countA)
+                nextSub(ay, ax+1, queueA, countA)
+            }
+            def callB() = {
+                nextSub(by-1, bx, queueB, countB)
+                nextSub(by+1, bx, queueB, countB)
+                nextSub(by, bx-1, queueB, countB)
+                nextSub(by, bx+1, queueB, countB)
+            }
+
+            if (N == "A"){
+                callA()
+                callB()
+            } else {
+                callB()
+                callA()
+            }
+        }
+        return board
+    }
+
+    val result = nextToPlot(Sh, AstartSearch(Sh), BstartSearch(Sh), (countMA, countMB))
+    
+    def resultCount(board: Array[Array[String]]): (Int, Int) = {
+        var cupA = 0
+        var cupB = 0
+        for (i <- 0 until H; j <- 0 until W){
+            if (countMA <= board(i)(j).toInt && board(i)(j).toInt < countMA + 10000) {
+                cupA = cupA + 1
+            }
+            if (countMB <= board(i)(j).toInt && board(i)(j).toInt < countMB + 10000) {
+                cupB = cupB + 1
+            }
+        }
+        return (cupA, cupB)
+    }
+    def printOut(A: Int, B: Int) = {
+        println(s"${A} ${B}")
+        if(A > B){
+            println("A")
+        } else if(A < B){
+            println("B")
+        }
+    }
+
+    val (x, y) = resultCount(result)
+    printOut(x, y)
 }
--- Bランク獲得/新・Bランクレベルアップメニュー/[要確認]【配列 2】立体で計算.scala
+++ Bランク獲得/新・Bランクレベルアップメニュー/[要確認]【配列 2】立体で計算.scala
@@ -3,31 +3,81 @@ import scala.io.StdIn._
 object Main extends App {
 
     val N = readLine().toInt
-    val Aline = Array.ofDim[Int](N, N, N)
-    val Bline = Array.ofDim[Int](N, N)
-    val Cline = Array.ofDim[Int](2, N)
-    val result = Array.ofDim[Int](2*N+2)
+    val Acube = Array.ofDim[Int](N, N, N)
+    var ans = 0 
 
-    for (i <- 0 until N){
-        Aline(i) = readLine().split(" ").map(_.toInt)
-        result(i) = Aline(i).sum
-    }
     for (i <- 0 until N; j <- 0 until N){
-        Bline(i)(j) = Aline(j)(i)
+        Acube(i)(j) = readLine().split(" ").map(_.toInt)
     }
-    for (i <- 0 until N){
-        Cline(0)(i) = Aline(i)(i)
+    
+    for (x <- 0 until N; i <- 0 until N){
+        var rowSum = 0
+        for (j <- 0 until N){
+            rowSum = rowSum + Acube(x)(i)(j)
+        }
+        ans = Array(rowSum, ans).max
     }
-    for (i <- 0 until N){
-        Cline(1)(i) = Aline(i)(N-1-i)
+    for (y <- 0 until N; i <- 0 until N){
+        var rowSum = 0
+        var colSum = 0
+        for (j <- 0 until N){
+            rowSum = rowSum + Acube(i)(y)(j)
+            colSum = colSum + Acube(j)(y)(i)
+        }
+        ans = Array(rowSum, colSum, ans).max
+    }
+    for (z <- 0 until N; i <- 0 until N){
+        var rowSum = 0
+        var colSum = 0
+        for (j <- 0 until N){
+            rowSum = rowSum + Acube(i)(j)(z)
+            colSum = colSum + Acube(j)(i)(z)
+        }
+        ans = Array(rowSum, colSum, ans).max
     }
 
-    for (i <- 0 until N){
-        result(N+i) = Bline(i).sum
+    for (x <- 0 until N){
+        var leftRightDown = 0
+        var leftRightUp = 0
+        for (i <- 0 until N){
+            leftRightDown = leftRightDown + Acube(x)(i)(i)
+            leftRightUp = leftRightUp + Acube(x)(N-1-i)(i)
+        }
+        ans = Array(leftRightDown, leftRightUp, ans).max
     }
-    for (i <- 0 until 2){
-        result(2*N+i) = Cline(i).sum
+    for (y <- 0 until N){
+        var leftRightDown = 0
+        var leftRightUp = 0
+        for (i <- 0 until N){
+            leftRightDown = leftRightDown + Acube(i)(y)(i)
+            leftRightUp = leftRightUp + Acube(N-1-i)(y)(i)
+        }
+        ans = Array(leftRightDown, leftRightUp, ans).max
+    }
+    for (z <- 0 until N){
+        var leftRightDown = 0
+        var leftRightUp = 0
+        for (i <- 0 until N){
+            leftRightDown = leftRightDown + Acube(i)(i)(z)
+            leftRightUp = leftRightUp + Acube(i)(N-1-i)(z)
+        }
+        ans = Array(leftRightDown, leftRightUp, ans).max
     }
 
-    println(result.max)
+    var l1 = 0
+    var l2 = 0
+    var l3 = 0
+    var l4 = 0
+
+    for (i <- 0 until N){
+        l1 = l1 + Acube(i)(i)(i)
+        l2 = l2 + Acube(i)(N-1-i)(N-1-i)
+        l3 = l3 + Acube(i)(N-1-i)(i)
+        l4 = l4 + Acube(i)(i)(N-1-i)
+    }
+    ans = Array(l1, l2, l3, l4, ans).max
+    println(ans)  
 }
+
+//解答例使用済み
+//意味は分からない
