72cce3eebc038ea028638c48489bab554f9bfb94
k433386 <k433386@kansai-u.ac.jp>
2023-05-18 15:17:06 +0900

    test
    test

--- Aランク獲得/Aランクレベルアップメニュー/連結の判定/STEP 2 隣接リスト.scala
+++
@@ -1,7 +0,0 @@
-import scala.io.StdIn._
-
-object Main extends App {
-
-    val s = readLine()
-    println(s(0))
-}
--- Aランク獲得/Aランクレベルアップメニュー/連結の判定/STEP 4 重みあり有向グラフの隣接行列と隣接リスト.scala
+++
@@ -1,8 +0,0 @@
-import scala.io.StdIn._
-
-object Main extends App {
-
-    val s = readLine().split("")
-
-    println(s"${s(0)} ${s(1)}")
-}
