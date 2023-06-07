2a7b5b06f56671c497811ba572da09d01dda0de0
k433386 <k433386@kansai-u.ac.jp>
2023-06-07 14:53:35 +0900

    test
    test

--- 定番アルゴリズムの習得/巡回セールスマン問題メニュー/禁断探索法によるTSP/禁断探索法によるTSP.scala
+++ 定番アルゴリズムの習得/巡回セールスマン問題メニュー/禁断探索法によるTSP/禁断探索法によるTSP.scala
@@ -1,18 +1,100 @@
 import scala.io.StdIn._
-import scala.collection.mutable.Queue
+import scala.math._
+import scala.collection.mutable.ArrayDeque
 
 object Main extends App {
 
-    val Q = readLine().toInt
-    val A = Queue[Int]()
-
-    for (_ <- 0 until Q){
-        val line = readLine().split(" ")
-        if (line(0) == "1"){
-            A.enqueue(line(1).toInt)
-        } else {
-            A.dequeue
-        }
-        println(A.mkString(" "))
+    def euclidean(a: Array[Double], b: Array[Double]):Double = {
+        return sqrt(pow(a(0)-b(0), 2) + pow(a(1)-b(1), 2))
     }
+    def calcLength(tour: Array[Int], n: Int, points: Array[Array[Double]], cnt: Int, length: Double): Double = {
+        if (cnt == n){
+            return length
+        } else {
+            calcLength(tour, n, points, cnt+1, length + euclidean(points(tour(cnt)), points(tour((cnt + 1) % n))))
+        }
+    }
+    def tabuSearch(tour: Array[Int], q: Int, tl: Int, n: Int, points: Array[Array[Double]], cnt: Int): Array[Int] = {
+        val tourBest = tour.clone()
+        var length = calcLength(tourBest, n, points, 0, 0.0)
+        val tabuList = ArrayDeque[Int]()
+
+        for (i <- 0 until q) {
+            def bestABsearch(a: Int, b: Int, bestA: Int, bestB: Int, diff: Double):(Int, Int) = {
+                if (a == n){
+                    return (a, b)
+                } else if (tabuList.contains(tour(a))){
+                    bestABsearch(a+1, b, bestA, bestB, diff)
+                } else if (b == n){
+                    bestABsearch(a+1, a+1+2, bestA, bestB, diff)
+                } else if (a == 0 && b == n - 1){
+                    bestABsearch(a, b+1, bestA, bestB, diff)
+                } else {
+                    val dBefore = euclidean(points(tour(a)), points(tour((a + 1) % n))) + euclidean(points(tour(b)), points(tour((b + 1) % n)))
+                    val dAfter = euclidean(points(tour(a)), points(tour(b))) + euclidean(points(tour((a + 1) % n)), points(tour((b + 1) % n)))
+                    val tmpDiff = dAfter - dBefore
+                    if (tmpDiff < diff) {
+                        bestABsearch(a, b+1, a, b, tmpDiff)
+                    } else {
+                        bestABsearch(a, b+1, bestA, bestB, diff)
+                    }
+                }
+            }
+            val (aBest, bBest) = bestABsearch(0, 0+2, 0, 0, 99999999.0)
+
+           tabuList.append(tour(aBest))
+            if (tabuList.length > tl) {
+                tabuList.removeHead()
+            }
+            val line = tour.take(aBest + 1) ++ tour.slice(aBest + 1, bBest + 1).reverse ++ tour.drop(bBest + 1)
+
+            for (i <- 0 until tour.length){
+                tour(i) = line(i)
+            }
+
+            val tabLength = calcLength(tourBest, n, points, 0, 0.0)
+            if (tabLength < length) {
+                length = tabLength
+                tourBest.indices.foreach(i => tourBest(i) = tour(i))
+            }
+        }
+        return tour
+    }
+
+    val Array(n, q, tl) = readLine().trim().split(" ").map(_.toInt)
+    val points = Array.ofDim[Double](n, 2)
+    for (i <- 0 until n) {
+        points(i) = readLine().trim().split(" ").map(_.toDouble)
+    }
+
+    val tour = Array.ofDim[Int](n)
+    for (i <- 0 until n) {
+        tour(i) = readLine().trim().toInt
+    }
+
+    val result = tabuSearch(tour, q, tl, n, points, 0)
+    result.foreach(println)
 }
+//解答例使用済み
+
+/*
+            for (a <- 0 until n) {
+                if (tabuList.contains(tour(a))) {
+                    
+                } else {
+                    for (b <- a + 2 until n) {
+                        if (a == 0 && b == n - 1) {
+                            
+                        } else {
+                            val dBefore = euclidean(points(tour(a)), points(tour((a + 1) % n))) + euclidean(points(tour(b)), points(tour((b + 1) % n)))
+                            val dAfter = euclidean(points(tour(a)), points(tour(b))) + euclidean(points(tour((a + 1) % n)), points(tour((b + 1) % n)))
+                            val tmpDiff = dAfter - dBefore
+                            if (tmpDiff < diff) {
+                                diff = tmpDiff
+                                val (aBest, bBest) = (a, b)
+                            }
+                        }
+                    }
+                }
+            }
+*/
